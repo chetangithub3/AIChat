74d273bcb1c20035dab71267efb81488f91ebd7d
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2025-12-08 15:07:22 +0530

    Chat bubble view

---
+++ AIChat/Core/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  AIChat
+//
+//  Created by Chetan Dhowlaghar on 12/8/25.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
---
+++ AIChat/Core/Tab Bar/Chats Stack/ChatBubbleView/ChatBubbleView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatBubbleView.swift
+//  AIChat
+//
+//  Created by Chetan Dhowlaghar on 12/8/25.
+//
+
+import SwiftUI
+
+struct ChatBubbleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatBubbleView()
+}
