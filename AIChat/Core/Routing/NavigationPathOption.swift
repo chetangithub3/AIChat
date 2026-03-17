75b928f272b6f01e1a2791fb5b2c333e029860ab
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2026-03-17 22:52:43 +0530

    routing added

---
+++ AIChat/Core/Routing/NavigationPathOption.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+enum NavigationPathOption: Hashable {
+    case chat(avatarId: String)
+    case category(category: CharacterOption, imageName: String)
+}
+
+extension View {
+    func navigationDestinationForCoreModules(path: Binding<[NavigationPathOption]>) -> some View {
+        self
+            .navigationDestination(for: NavigationPathOption.self) { newValue in
+                switch newValue {
+                    case .chat(avatarId: let avatarId):
+                        ChatView(avatarId: avatarId)
+                    case .category(category: let category, let imageName):
+                        CategoryListView(path: path, category: category, imageName: imageName)
+                }
+            }
+    }
+}
