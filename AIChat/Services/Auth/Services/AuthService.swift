8816300db51dc1d8175cc249dd864a830b8ccbe3
Chetan Dhowlaghar <chetan.getsmail@gmail.com>
2026-03-20 04:44:38 +0530

    MockAuth Service

---
+++ AIChat/Services/Auth/Services/AuthService.swift
@@ -0,0 +1,7 @@
+protocol AuthService: Sendable {
+    func getAuthenticatedUser() -> UserAuthInfo?
+    func signInAnonymously() async throws -> (user: UserAuthInfo, isNewUser: Bool)
+    func signInApple() async throws -> (user: UserAuthInfo, isNewUser: Bool)
+    func signOut() throws
+    func deleteAccount() async throws
+}
---
+++ AIChat/Services/Auth/Services/MockAuthService.swift
@@ -0,0 +1,26 @@
+struct MockAuthService: AuthService {
+    let currentUser: UserAuthInfo?
+
+    init(user: UserAuthInfo? = nil) {
+        self.currentUser = user
+    }
+    func getAuthenticatedUser() -> UserAuthInfo? {
+        currentUser
+    }
+
+    func signInAnonymously() async throws -> (user: UserAuthInfo, isNewUser: Bool) {
+        let user = UserAuthInfo.mock(isAnonymous: true)
+        return (user, true)
+    }
+
+    func signInApple() async throws -> (user: UserAuthInfo, isNewUser: Bool) {
+        let user = UserAuthInfo.mock(isAnonymous: false)
+        return (user, false)
+    }
+
+    func signOut() throws {
+    }
+
+    func deleteAccount() async throws {
+    }
+}
